87645077b06083b44fe2df6e76190dbf65be6414
Wow1derful <39995649+Wow1derful@users.noreply.github.com>
2019-03-25 01:32:44 +0900

    #2.22 Home Controller part Two

--- db.js
+++ db.js
@@ -4,8 +4,9 @@ export const videos = [
       title: "Video awesome",
       description: "This is something I love",
       views: 24,
-      videoFile: "https://archive.org/details/BigBuckBunny_124",
-      creator: {
+      videoFile:
+       "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
         id: 121212,
         name: "Nicolas",
         email: "nico@las.com"
@@ -16,8 +17,9 @@ export const videos = [
       title: "Video super",
       description: "This is something I love",
       views: 24,
-      videoFile: "https://archive.org/details/BigBuckBunny_124",
-      creator: {
+      videoFile: 
+       "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
         id: 121212,
         name: "Nicolas",
         email: "nico@las.com"
@@ -28,8 +30,9 @@ export const videos = [
       title: "Video nice",
       description: "This is something I love",
       views: 24,
-      videoFile: "https://archive.org/details/BigBuckBunny_124",
-      creator: {
+      videoFile: 
+       "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
         id: 121212,
         name: "Nicolas",
         email: "nico@las.com"
@@ -40,8 +43,9 @@ export const videos = [
       title: "Video perfect",
       description: "This is something I love",
       views: 24,
-      videoFile: "https://archive.org/details/BigBuckBunny_124",
-      creator: {
+      videoFile: 
+       "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
         id: 121212,
         name: "Nicolas",
         email: "nico@las.com"
