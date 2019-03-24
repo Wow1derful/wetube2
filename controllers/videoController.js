9d6eecb968cda7381f30e2ba83907d6f98595528
Wow1derful <39995649+Wow1derful@users.noreply.github.com>
2019-03-25 00:33:29 +0900

    #2.18 Search Controller

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,11 +1,20 @@
 export const home = (req, res) => res.render("Home", { pageTile: "Home"});
-export const search = (req, res) => 
-    res.render("Search", { pageTile: "Search"});
+
+export const search = (req, res) => {
+    const {
+        query: { term: searchingBy }
+    } = req;
+    res.render("search", { pageTile: "Search", searchingBy});
+};
+
 export const upload = (req, res) => 
-    res.render("Upload", { pageTile: "Upload"});
+    res.render("upload", { pageTile: "Upload"});
+
 export const videoDetail = (req, res) => 
-    res.render("Video Detail", { pageTile: "Video Detail"});
-export const editVideo = (req, res) => 
-    res.render("Edit Video", { pageTile: "Edit Video"});
-export const deleteVideo = (req, res) => 
-    res.render("Delete Video", { pageTile: "Delete Video"});
+    res.render("videoDetail", { pageTile: "Video Detail"});
+
+    export const editVideo = (req, res) => 
+    res.render("editVideo", { pageTile: "Edit Video"});
+
+    export const deleteVideo = (req, res) => 
+    res.render("deleteVideo", { pageTile: "Delete Video"});
