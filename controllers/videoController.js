12d8748fdc5ebc7e6f76101c8ba01186ef042cd9
Wow1derful <39995649+Wow1derful@users.noreply.github.com>
2019-07-24 23:32:59 +0900

    3.6 Uploading and Creating a Video_2

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -4,7 +4,7 @@ import Video from "../models/Video";
 export const home = async (req, res) => {
     try{
         const videos = await Video.find({});
-        req.render("home", { pageTitle: "Home", videos });
+        res.render("home", { pageTitle: "Home", videos });
     } catch (error) {
         console.log(error);
         res.render("home", { pageTitle: "Hone", Videos: [] })
