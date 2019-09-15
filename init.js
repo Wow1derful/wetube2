c88da46510189d5a3c8cc5f18c84b4b31e81e49b
Wow1derful <39995649+Wow1derful@users.noreply.github.com>
2019-09-15 23:30:09 +0900

    .1 Local Authentication with Passport part One

--- init.js
+++ init.js
@@ -1,13 +1,15 @@
+import dotdenv from "dotenv";
 import "./db";
 import app from "./app";
-import dotdenv from "dotenv";
+
 dotdenv.config();
+
 import "./models/Video";
 import "./models/Comment";
-
+import "./models/User";
 const PORT = process.env.PORT || 4000;
 
-const handleListening = () => 
-    console.log(`✅  Listening on : http://localhost:${PORT}`);
+const handleListening = () =>
+  console.log(`✅  Listening on : http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);
