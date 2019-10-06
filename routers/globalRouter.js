100a0afed73e1fc24000b25c4097887efc831bab
Wow1derful <39995649+Wow1derful@users.noreply.github.com>
2019-10-06 23:27:48 +0900

    Github Log In part Two_2

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,6 +1,6 @@
 import express from "express";
-import routes from "../routes";
 import passport from "passport";
+import routes from "../routes";
 import { home, search } from "../controllers/videoController";
 import {
   getJoin,
